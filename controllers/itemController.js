ee4856e6241a1306e9377c5a45833aeeb9c6ac88
Hannah Kim <67137839+hannahkim313@users.noreply.github.com>
2024-05-14 10:31:04 -0700

    Add a semantic <hr> tag in the sidebar, update the Item controller function to fetch all Item records, create and render the item list page, and add styles.

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -20,7 +20,13 @@ exports.index = asyncHandler(async (req, res, next) => {
 
 // Display list of all items
 exports.item_list = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Item list');
+  // Fetch all Item records
+  const allItems = await Item.find({}, 'name').sort({ name: 1 }).exec();
+
+  res.render('item-list', {
+    title: 'All Items',
+    item_list: allItems,
+  });
 });
 
 // Display detail page for a specific item
