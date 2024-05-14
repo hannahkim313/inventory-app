09ca0a20597ce9cefd97e02673a2d451f5731bbe
Hannah Kim <67137839+hannahkim313@users.noreply.github.com>
2024-05-14 10:51:37 -0700

    Update the Category controller function to fetch all Category records, create and render the category list page, and add styles.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,9 +1,18 @@
 const Category = require('../models/category');
+
 const asyncHandler = require('express-async-handler');
 
 // Display list of all categories
 exports.category_list = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Category list');
+  // Fetch all Category records
+  const allCategories = await Category.find({}, 'name')
+    .sort({ name: 1 })
+    .exec();
+
+  res.render('category_list', {
+    title: 'All Categories',
+    category_list: allCategories,
+  });
 });
 
 // Display detail page for a specific category
