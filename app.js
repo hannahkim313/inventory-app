99b2a1150b9c620b796f7f8b84655d7cf230c25f
Hannah Kim <67137839+hannahkim313@users.noreply.github.com>
2024-05-22 21:33:53 -0700

    Update express-validator package and change max number of requests for rate limiter

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 const RateLimit = require('express-rate-limit');
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 20,
+  max: 35,
 });
 // Apply rate limiter to all requests
 app.use(limiter);
